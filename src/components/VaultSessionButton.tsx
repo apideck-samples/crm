4787aad5b2f9d8d5dd206174583096018100cc05
Jake Prins <jakeprins@outlook.com>
2021-03-17 15:21:22 +0100

    Implement vault session button to redirect to vault

---
+++ src/components/VaultSessionButton.tsx
@@ -0,0 +1,32 @@
+import { Button } from '@apideck/components'
+import { FC, useState } from 'react'
+
+const VaultSessionButton: FC = () => {
+  const [isLoading, setIsLoading] = useState(false)
+
+  const redirectToVault = async () => {
+    setIsLoading(true)
+    const response = await createSession()
+    if (response.data.session_uri) window.location.href = response.data.session_uri
+    setIsLoading(false)
+  }
+  const createSession = async () => {
+    const response = await fetch('/api/vault/sessions', {
+      method: 'POST',
+      body: JSON.stringify({ redirect_uri: 'http://localhost:3000/' })
+    })
+
+    return response.json()
+  }
+
+  return (
+    <Button
+      text="Add connection"
+      className="inline-flex ml-3"
+      isLoading={isLoading}
+      onClick={() => redirectToVault()}
+    />
+  )
+}
+
+export default VaultSessionButton

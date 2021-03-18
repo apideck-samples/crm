5fd2fa807c58c814027f709a04e23ec0951d0ba0
Jake Prins <jakeprins@outlook.com>
2021-03-18 13:46:09 +0100

    Add sidebar layout component

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,28 @@
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center h-64">
+      <svg
+        className="w-8 h-8 mr-3 -ml-1 text-indigo-600 animate-spin"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          className="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          strokeWidth="4"
+        ></circle>
+        <path
+          className="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
+  )
+}
+
+export default Spinner

671f367b3b3959caacaa3bbccebd19df0790da9d
Jake Prins <jakeprins@outlook.com>
2021-03-22 14:18:00 +0100

    Add node-fetch for serverless functions

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -3,6 +3,12 @@ import { render, screen, waitFor } from '@testing-library/react'
 import App from 'pages/index'
 
 describe('App', () => {
+  global.fetch = jest.fn(() =>
+    Promise.resolve({
+      json: () => Promise.resolve({})
+    })
+  ) as any
+
   it('matches the snapshot', async () => {
     render(<App />)
     await waitFor(() => {
--- src/pages/api/crm/leads/patch.js
+++ src/pages/api/crm/leads/patch.js
@@ -1,3 +1,4 @@
+import fetch from 'node-fetch'
 import { headers } from '../../_utils'
 
 module.exports = async (req, res) => {
